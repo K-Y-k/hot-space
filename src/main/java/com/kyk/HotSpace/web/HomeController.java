2818ef7e9d901fb88b013d77eef56ea1a9a924e3
Kim-Yong-Kyeong <kyk4957@naver.com>
2024-05-13 16:37:23 +0900

    프로젝트 환경 설정

---
+++ src/main/java/com/kyk/HotSpace/web/HomeController.java
@@ -0,0 +1,16 @@
+package com.kyk.HotSpace.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "main";
+    }
+}
